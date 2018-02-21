9693a6889233d9ebdb9ef5784257afefc78ea06a
Dan Oneata <dan.oneata@gmail.com>
2018-02-21 21:44:49 +0200

    MINOR Small changes

--- nca.py
+++ nca.py
@@ -5,6 +5,9 @@ import numpy as np
 from scipy.optimize import (
     check_grad,
     fmin_cg,
+    fmin_ncg,
+    fmin_bfgs,
+)
 
 from sklearn.base import (
     BaseEstimator,
@@ -30,6 +33,29 @@ def square_dist(x1, x2=None):
 
 
 def nca_cost(A, xx, yy, reg):
+    """Neighbourhood Components Analysis: cost function and gradients
+
+        ff, gg = nca_cost(A, xx, yy)
+
+    Evaluate a linear projection from a D-dim space to a K-dim space (K<=D).
+    See Goldberger et al. (2004).
+
+    Inputs:
+        A  KxD Current linear transformation.
+        xx NxD Input data
+        yy Nx1 Corresponding labels, taken from any discrete set
+
+    Outputs:
+        ff 1x1 NCA cost function
+        gg KxD partial derivatives of ff wrt elements of A
+
+    Motivation: gradients in existing implementations, and as written in the
+    paper, have the wrong scaling with D. This implementation should scale
+    correctly for problems with many input dimensions.
+
+    Note: this function should be passed to a MINIMIZER.
+
+    """
 
     N, D = xx.shape
     assert(yy.size == N)
@@ -47,7 +73,6 @@ def nca_cost(A, xx, yy, reg):
     np.fill_diagonal(ss, np.inf)
     mm = np.min(ss, axis=0)
     kk = np.exp(mm - ss)  # NxN
-    # kk = np.exp(-ss)  # NxN
     np.fill_diagonal(kk, 0)
     Z_p = np.sum(kk, 0)  # N,
     p_mn = kk / Z_p[np.newaxis, :]  # P(z_m | z_n), NxN
@@ -97,33 +122,6 @@ def nca_cost_batch(self, A, xx, yy, idxs):
 
 
 class NCA(BaseEstimator, TransformerMixin):
-    """Neighbourhood Components Analysis: cost function and gradients
-
-        ff, gg = nca_cost(A, xx, yy)
-
-    Evaluate a linear projection from a D-dim space to a K-dim space (K<=D).
-    See Goldberger et al. (2004).
-
-    Inputs:
-        A KxD Current linear transformation.
-        xx NxD Input data
-        yy Nx1 Corresponding labels, taken from any discrete set
-
-    Outputs:
-        ff 1x1 NCA cost function
-        gg KxD partial derivatives of ff wrt elements of A
-
-    Motivation: gradients in existing implementations, and as written in the
-    paper, have the wrong scaling with D. This implementation should scale
-    correctly for problems with many input dimensions.
-
-    Note: this function should be passed to a MINIMIZER.
-
-    TODO Numerical stability. Currently if the scale of A is too large, so that
-    a whole column of kk underflows, Bad Things happen.
-
-    """
-
     def __init__(self, reg=0, dim=None, optimizer='cg'):
         self.reg = reg
         self.K = dim
@@ -145,21 +143,21 @@ class NCA(BaseEstimator, TransformerMixin):
         if self.K is None:
             self.K = D
 
-        self.A = 0.5 * np.random.randn(self.K, D)
+        self.A = np.random.randn(self.K, D) / np.sqrt(N)
 
         X = self.standard_scaler.fit_transform(X)
         return self._fit(X, y)
 
     def _fit_gradient_descent(self, X, y):
         # Gradient descent.
-        self.learning_rate = 0.1
+        self.learning_rate = 0.001
         self.error_tol = 0.001
         self.max_iter = 1000
 
         curr_error = None
 
         # print(check_grad(costf, costg, 0.1 * np.random.randn(self.K * D)))
-        idxs = list(sorted(random.sample(range(len(X)), 100)))
+        # idxs = list(sorted(random.sample(range(len(X)), 100)))
 
         for it in range(self.max_iter):
 
@@ -169,7 +167,7 @@ class NCA(BaseEstimator, TransformerMixin):
             prev_error = curr_error
             curr_error = f
 
-            print('{:4d} {:+6.2f}'.format(it, curr_error))
+            print('{:4d} {:+.6f}'.format(it, curr_error))
 
             if prev_error and np.abs(curr_error - prev_error) < self.error_tol:
                 break
@@ -188,7 +186,8 @@ class NCA(BaseEstimator, TransformerMixin):
             return g.ravel()
 
         # print(check_grad(costf, costg, 0.1 * np.random.randn(self.K * D)))
-        self.A = fmin_cg(costf, self.A.ravel(), costg).reshape([self.K, D])
+        self.A = fmin_cg(costf, self.A.ravel(), costg, maxiter=400)
+        self.A = self.A.reshape([self.K, D])
         return self
 
     def fit_transform(self, X, y):
